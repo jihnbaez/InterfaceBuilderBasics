7c4baa4ebfbb92e981f7cb15533c9a4adaf4203a
John <baezjohn312@gmail.com>
2022-09-15 13:37:08 -0400

    Initial Commit

---
+++ InterfaceBuilderBasics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterfaceBuilderBasics
+//
+//  Created by 8h on 9/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
